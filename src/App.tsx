6df6a83aca55171c814c1b32a87de78493235300
Thomas Reay <git@thomasreay.info>
2021-09-09 21:59:29 +0900

    Add particles background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import Particles from "react-tsparticles";
+
 import DescriptionIcon from "@material-ui/icons/Description";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import Email from "@material-ui/icons/Email";
 import Popper from "@material-ui/core/Popper";
 import IconButton, { IconButtonProps } from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
@@ -10,12 +13,23 @@ import { SvgIconProps } from "@material-ui/core/SvgIcon";
 import resume from "./static/pdfs/resume.pdf";
 import headshot from "./static/images/headshot.jpg";
 
+import particlesOptions from "./particles-options.json";
+
+const ParticlesBackground = styled(Particles)`
+  position: fixed;
+  width: 100%;
+  height 100%;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
+
 const AppBody = styled.div`
-  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  height: 100vh;
 `;
 
 const InfoCard = styled.div`
@@ -26,7 +40,6 @@ const InfoCard = styled.div`
   justify-content: center;
   color: #f8f8f2;
 
-  background-color: #282a36;
   border-radius: 2px;
 `;
 
@@ -82,31 +95,42 @@ function TooltipButton<C extends React.ElementType>({
 
 function App() {
   return (
-    <AppBody>
-      <InfoCard>
-        <Headshot src={headshot} alt="headshot" />
-        <h1>Thomas Reay</h1>
-        <ItalicH3>Software Developer</ItalicH3>
-        <ButtonBar>
-          <TooltipButton
-            title="Github"
-            Icon={GitHubIcon}
-            href="https://www.github.com"
-          />
-          <TooltipButton
-            title="LinkedIn"
-            Icon={LinkedInIcon}
-            href="https://www.linkedin.com"
-          />
-          <TooltipButton
-            title="Resume"
-            Icon={DescriptionIcon}
-            href={resume}
-            target="blank"
-          />
-        </ButtonBar>
-      </InfoCard>
-    </AppBody>
+    <>
+      <ParticlesBackground options={particlesOptions} />
+      <AppBody>
+        <InfoCard>
+          <Headshot src={headshot} alt="headshot" />
+          <h1>Thomas Reay</h1>
+          <ItalicH3>Software Developer</ItalicH3>
+          <ButtonBar>
+            <TooltipButton
+              title="Github"
+              Icon={GitHubIcon}
+              href="https://www.github.com/tmreay"
+              target="_blank"
+            />
+            <TooltipButton
+              title="LinkedIn"
+              Icon={LinkedInIcon}
+              href="https://www.linkedin.com/in/thomas-reay-722167103/"
+              target="_blank"
+            />
+            <TooltipButton
+              title="Email"
+              Icon={Email}
+              href="mailto:&#099;&#111;&#110;&#116;&#097;&#099;&#116;&#064;&#116;&#104;&#111;&#109;&#097;&#115;&#114;&#101;&#097;&#121;&#046;&#105;&#110;&#102;&#111;"
+              target="_blank"
+            />
+            <TooltipButton
+              title="Resume"
+              Icon={DescriptionIcon}
+              href={resume}
+              target="_blank"
+            />
+          </ButtonBar>
+        </InfoCard>
+      </AppBody>
+    </>
   );
 }
 
