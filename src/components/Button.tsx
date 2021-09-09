5c90a9a70dd99260e75246c208a0aa71a34f5990
Thomas Reay <git@thomasreay.info>
2021-09-09 21:59:28 +0900

    Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import Popper from "@material-ui/core/Popper";
+import { SvgIconProps } from "@material-ui/core/SvgIcon";
+
+const CustomPopper = styled(Popper)`
+  .MuiTooltip-tooltip {
+    font-family: inherit;
+  }
+`;
+
+type ButtonProps = {
+  title: string;
+  Icon: React.ComponentType<SvgIconProps>;
+  onClick?: () => void;
+};
+
+function Button({ title, Icon, onClick }: ButtonProps) {
+  return (
+    <Tooltip
+      PopperComponent={CustomPopper}
+      title={title}
+      arrow
+      onClick={(e) => {
+        e.preventDefault();
+        onClick && onClick();
+      }}
+    >
+      <IconButton>
+        <Icon />
+      </IconButton>
+    </Tooltip>
+  );
+}
+
+export default Button;
